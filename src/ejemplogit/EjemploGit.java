e9305b8fd61e5126e3e0edfef1e2ca38f421d900
Merle66 <merle6665@gmail.com>
2018-04-09 09:09:13 +0200

    this. primeras modificaciones

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -6,6 +6,10 @@ public class EjemploGit {
         
         System.out.println("Hola mundo");
 
+                       
+        System.out.println("Adiós mundo");
+        
+        System.out.println("Esto está mal");
     }
     
 }
